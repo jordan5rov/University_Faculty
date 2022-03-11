0789a2d876f0625b94b0ad9f7391856e7cade018
jordan5rov <jordan5rov@gmail.com>
2022-03-11 20:20:05 +0200

    Added base.html and static files with styles and scripts

--- University_Faculty/settings.py
+++ University_Faculty/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'University_Faculty.guest',
 ]
 
 MIDDLEWARE = [
@@ -126,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- University_Faculty/urls.py
+++ University_Faculty/urls.py
@@ -3,8 +3,11 @@
  ctrl + shift + k, ctrl + enter -> push
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = (
     path('admin/', admin.site.urls),
+    path('', include('University_Faculty.guest.urls'))
 )
+
