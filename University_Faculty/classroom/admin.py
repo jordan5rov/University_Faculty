d54c8b1947894c94d67b8a76ed44c6b1738facc8
jordan5rov <jordan5rov@gmail.com>
2022-04-27 23:28:01 +0300

    Added admin part of the project

--- University_Faculty/classroom/admin.py
+++ University_Faculty/classroom/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(UniversityUser)
+class UniversityUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_staff', 'is_active', 'is_superuser', 'last_login', 'date_joined')
+    list_display_links = ('id', 'username', 'email')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email')
+    list_per_page = 50
+    ordering = ('username',)
--- University_Faculty/web/admin.py
+++ University_Faculty/web/admin.py
@@ -5,19 +5,18 @@ from . import models
 # Register your models here.
 @admin.register(models.News)
 class NewsAdmin(admin.ModelAdmin):
-    # list_display = ('title', 'author', 'publish')
-    # list_filter = ('publish', 'author')
-    # search_fields = ('title', 'body')
-    # raw_id_fields = ('author',)
-    # date_hierarchy = 'publish'
-    # ordering = ('publish',)
+    list_display = ('title', 'image', 'description', 'published_on')
+    list_filter = ('published_on',)
+    search_fields = ('title',)
+    date_hierarchy = 'published_on'
+    ordering = ('published_on',)
     pass
+
 
 @admin.register(models.Event)
 class EventAdmin(admin.ModelAdmin):
-    # list_display = ('title', 'event_date', 'event_time', 'location')
-    # list_filter = ('event_date', 'location')
-    # search_fields = ('title', 'location')
-    # date_hierarchy = 'event_date'
-    # ordering = ('event_date',)
-    pass
+    list_display = ('title', 'date', 'image', 'description')
+    list_filter = ('date',)
+    search_fields = ('title',)
+    date_hierarchy = 'date'
+    ordering = ('date',)
